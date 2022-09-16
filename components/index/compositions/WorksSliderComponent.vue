011aca9b0a22c8af15909d1c639d520f961ffe7c
spook-QuQ <59325698+spook-QuQ@users.noreply.github.com>
2022-09-16 11:01:48 +0900

    fixed the slider structure and the style

--- components/index/compositions/WorksSliderComponent.vue
+++ components/index/compositions/WorksSliderComponent.vue
@@ -21,22 +21,33 @@
           :class="currentSliderIndex == worksData.length - 1 ? 'disabled' : ''"
         ): v-icon mdi-chevron-right
     .banner-slider-wrapper
-      .banner-slider(
+       v-row.banner-slider(
+        no-gutters
         ref="slider"
         :style="`transform: translateX(${ scrollPosition }px)`"
       )
-        .slider(v-for="work in worksData")
+        v-col.slider(
+          no-gutters
+          v-for="work in worksData"
+          cols="12"
+          md="4"
+          lg="4"
+          xl="4"
+        )
           NuxtLink(:to="`/works/` + work.id")
             .img-wrapper
               v-img(
                 :src="work.eyecatch"
                 :aspect-ratio="4 / 3"
               )
-            .head
-              time {{ new Date(work.publishedAt).toLocaleString().split(' ')[0] }}
-              h3 {{ work.title }}
-            span {{ work.content }}
-    div.text-center.mb-0.pt-4.pb-14.view-more-button
+                v-row.pa-2(no-gutters)
+                  v-spacer
+                  v-btn(icon color="white"): v-icon mdi-exit-to-app
+            //- .head
+            //-   time {{ new Date(work.publishedAt).toLocaleString().split(' ')[0] }}
+            //-   h3 {{ work.title }}
+            //- span {{ work.content }}
+    div.text-center.mb-0.pt-8.pb-14.view-more-button
       ViewMoreButton(url="/works" text="実績・事例をもっと見る")
 </template>
 
@@ -146,9 +157,8 @@ export default defineComponent({
             opacity: 0.3
     .banner-slider-wrapper
       position: relative
-      position: relative
-      +mediaMax(1000px)
-        margin-right: -50vw
+      // +mediaMax(1000px)
+      //   margin-right: -50vw
       &::before
         content: ""
         display: block
@@ -161,23 +171,25 @@ export default defineComponent({
         +mediaMax(1000px)
           width: 100%
       .banner-slider
-        display: flex
+        // display: flex
         flex-flow: nowrap
         transition: .3s
-        margin-right: -480px
+        // margin-right: -480px
         margin-left: 40px
         +mediaMax(1000px)
           margin-left: 40px
-        +mediaMax(416px)
+        +mediaMax(680px)
           margin-left: 0px
+        // +mediaMax(416px)
+        //   margin-left: 0px
         .slider
           margin-right: 40px
           max-width: 640px
-          +mediaMax(760px)
-            width: 80vw
-          +mediaMax(416px)
-            width: 100vw
-            padding: 8px
+          // +mediaMax(760px)
+          //   width: 80vw
+          // +mediaMax(416px)
+          //   width: 100vw
+          //   padding: 8px
           &:hover
             background: mix(mix(white, lightgray), transparent)
           a
@@ -186,18 +198,18 @@ export default defineComponent({
             overflow: hidden
             text-decoration: none
             // .img-wrapper
-            .head
-              margin-top: 16px
-              time
-                color: #8E9095
-              h3
-                color: mix(dimgray, black)
-                // color: black
-                margin-top: 16px
-                font-size: 24px
-                font-family: $font-family
-                +mediaMax(700px)
-                  font-size: 20px
+            // .head
+            //   margin-top: 16px
+            //   time
+            //     color: #8E9095
+            //   h3
+            //     color: mix(dimgray, black)
+            //     // color: black
+            //     margin-top: 16px
+            //     font-size: 24px
+            //     font-family: $font-family
+            //     +mediaMax(700px)
+            //       font-size: 20px
             span
               display: block
               margin-top: 16px
