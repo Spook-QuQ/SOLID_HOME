8c7e886222496a94ba070b00787933393d2cca73
spook-QuQ <spook-QuQ@users.noreply.github.com>
2022-09-28 05:25:53 +0000

    deploy: e2eeb350aa9b6d8c3e0d206521edf42a797109eb

--- _nuxt/static/1664327374/about_us/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSOLID_HOME\u002F_nuxt\u002Fstatic\u002F1664327374",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout_us",config:{_app:{basePath:"\u002FSOLID_HOME\u002F",assetsPath:"\u002FSOLID_HOME\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
--- _nuxt/static/1664327374/services/外構/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSOLID_HOME\u002F_nuxt\u002Fstatic\u002F1664327374",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002F%E5%A4%96%E6%A7%8B",config:{_app:{basePath:"\u002FSOLID_HOME\u002F",assetsPath:"\u002FSOLID_HOME\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
