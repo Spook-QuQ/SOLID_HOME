2b31ac755f1ff19b2df681f058b2698948b431e0
spook-QuQ <59325698+spook-QuQ@users.noreply.github.com>
2022-09-29 10:06:59 +0900

    added the cookie agreement window

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,16 +10,18 @@ v-app.root
   )
     span Scroll To Top
     v-icon mdi-arrow-up
+  div.cookie-agreement(v-if="!cookieAgreement")
+    v-col(cols="8"): p このウェブサイトでは、Cookieなどのデータを保管することで、アナリティクスやターゲティング、パーソナライゼーションなど、サイトの重要な機能を可能にしています。 このウェブサイトに留まることで、当該システムの使用に同意したものとみなされます。
+    v-col(cols="4"): button(@click="closeCookieAgreement") 分かりました
 </template>
 
 <script>
 export default {
   name: 'DefaultLayout',
-  data () {
-    return {
-      observer: null
-    }
-  },
+  data: () => ({
+    observer: null,
+    cookieAgreement: false
+  }),
   methods: {
     scrollToTop () {
       if (window) window.scrollTo({
@@ -27,6 +29,12 @@ export default {
         left: 0,
         behavior: 'smooth'
       })
+    },
+    closeCookieAgreement () {
+      this.cookieAgreement = true
+      if (localStorage) {
+        localStorage.cookieAgreement = 'close'
+      }
     }
   },
   mounted () {
@@ -42,6 +50,10 @@ export default {
       }
     })
     this.observer.observe(footer)
+
+    if (localStorage) {
+      if (localStorage.cookieAgreement === 'close') this.cookieAgreement = true
+    }
   },
   unmounted () {
     this.observer.disconnect()
@@ -63,4 +75,24 @@ export default {
       outline: solid 1px mix(black, dimgray)
       font-weight: bold
       font-family: $font-family
+    div.cookie-agreement
+      display: flex
+      justify-content: center
+      align-items: center
+      position: fixed
+      width: 100%
+      bottom: 0px
+      background: lightgray
+      color: mix(black, dimgray)
+      padding: 8px
+      box-shadow: 0px 16px 20px 8px black
+      p
+        margin-bottom: 0px
+        font-size: 14px
+      button
+        border: 4px solid rgba(dimgray, 0.3)
+        padding: 16px
+        font-weight: bold
+        color: rgba(dimgray, 0.8)
+        font-size: 14px
 </style>
