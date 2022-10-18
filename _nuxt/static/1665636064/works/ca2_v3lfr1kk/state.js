1c290511316168b55ba1d5dbb8a8e45f352fb931
spook-QuQ <spook-QuQ@users.noreply.github.com>
2022-10-18 03:49:38 +0000

    deploy: 9fc640f3a8ef33b2acd33ecdee26752f2182cf44

--- _nuxt/static/1665636064/works/ca2_v3lfr1kk/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665636064",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002Fca2_v3lfr1kk",config:{googleAnalytics:{id:"G-RLYNEYFJ1M"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
